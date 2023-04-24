30317d880ab2f64e60eabeecde2e2ada3c657c12
tim.walker-at-627858864654 <tim@rabbies.com>
2023-04-24 10:02:00 +0100

    faking OG image job class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,24 @@
 
 namespace Tests;
 
+use App\Jobs\CreateOgImageJob;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Bus;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Bus::fake([CreateOgImageJob::class]); // fakes this for any invocation of the job class
+        
+    }
 
     public function login(User $user = null)
     {
