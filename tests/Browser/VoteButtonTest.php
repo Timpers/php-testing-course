e0de91110a1355cd5d51ff0806951a45114cb0c9
Tim Hawk-Walker <tdub@tdub.co.uk>
2023-05-02 10:45:49 +0100

    dusk test

---
+++ tests/Browser/VoteButtonTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\BlogPost;
+use App\Http\Controllers\BlogPostController;
+
+class VoteButtonTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_vote_button_increments_counter()
+    {
+        #create a post
+
+        $post = BlogPost::factory()->create(['likes' => 10]);
+
+        #navigate to the post
+        $this->browse(function (Browser $browser) use ($post) {
+            $browser->visit(action([BlogPostController::class, 'show'], $post->slug))
+                ->with('@vote-button', function (Browser $button) {
+                    $button->assertSee(10);
+                })
+                ->click('@vote-button')                
+                ->pause(500)
+                ->screenshot('vote-button-clicked')
+                ->with('@vote-button', function (Browser $button) {
+                    $button->assertSee(11);
+                });
+        });
+
+        #check the likes counter
+
+        #click the vote button
+
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSee('My Blog');
+        });
+    }
+}
